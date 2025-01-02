209f524696f01a58feb0136931d037e06253e902
FachmiR4 <fachmiramadhan.fr@gmail.com>
2025-01-02 15:07:40 +0700

    seeder and factory

---
+++ database/factories/AnggotaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Anggota>
+ */
+class AnggotaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+                'name' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'no_hp' => fake()->number_format(),
+                'alamat' => fake()->sentence(),
+        ];
+    }
+}
